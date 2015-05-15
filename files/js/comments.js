3ddee59a50b2974a218c4a480b4544418dfc7caf
Chris Grimmett <xtoast@gmail.com>
2015-05-15 09:31:45 +0000

    added comments

---
+++ files/js/comments.js
@@ -0,0 +1,11 @@
+/* * * CONFIGURATION VARIABLES * * */
+var disqus_shortname = 'teamrpc';
+var disqus_identifier = 'index_05152015';
+var disqus_url = 'http://www.teamrpc.info';
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
